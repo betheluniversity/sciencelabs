2c158af88e60496427218eb18c9c6a1e46bfeaf8
c-schwarze <ces55739@bethel.edu>
2016-10-14 11:12:40 -0500

    Added a fix to make sure sessions only send 1 email if you click the box multiple times.

--- src/Bethel/SessionViewBundle/Services/SessionClose.php
+++ src/Bethel/SessionViewBundle/Services/SessionClose.php
@@ -25,47 +25,51 @@ class SessionClose {
      * @return Session
      */
     public function close(Session $session, $comment = null) {
-        $this->session = $session;
-        $this->session->setEndTime(new \DateTime("now"));
-        $this->session->setOpen(false);
-        if($comment) {
-            $this->session->setComments($comment);
-        }
-        $this->em->persist($this->session);
-        $this->em->flush();
 
-        /** @var $userRepository \Bethel\EntityBundle\Entity\UserRepository */
-        $userRepository = $this->em->getRepository('BethelEntityBundle:User');
-        $emailRecipients = $userRepository->findBy(array('sendEmail' => true));
-        /** @var $sessionEmailer \Bethel\ReportViewBundle\Services\SessionEmail */
-        /** @var \Bethel\EntityBundle\Entity\User $emailRecipient */
-        foreach($emailRecipients as $emailRecipient) {
-            // Make sure we're only sending to admins and professors
-            if($this->userHasRole($emailRecipient,'ROLE_ADMIN')) {
-                $sessionEmailer = $this->sessionEmail->create($session, $emailRecipient);
-                $sessionEmailer->sendEmail(null, true);
-            } elseif($this->userHasRole($emailRecipient,'ROLE_PROFESSOR')) {
-                /** @var \Bethel\EntityBundle\Entity\CourseRepository $courseRepository */
-                $courseRepository = $this->em->getRepository('BethelEntityBundle:Course');
+        if( $session->getOpen() ) {
+            $this->session = $session;
+            $this->session->setEndTime(new \DateTime("now"));
+            $this->session->setOpen(false);
+            if ($comment) {
+                $this->session->setComments($comment);
+            }
+            $this->em->persist($this->session);
+            $this->em->flush();
 
-                $reportCourses = array();
-                $sessionCourses = $courseRepository->getAttendedSessionCourses($session);
-                foreach( $sessionCourses as $course){
-                    if( $course->getProfessors()->contains($emailRecipient) )
-                        array_push($reportCourses, $course);
-                }
+            /** @var $userRepository \Bethel\EntityBundle\Entity\UserRepository */
+            $userRepository = $this->em->getRepository('BethelEntityBundle:User');
+            $emailRecipients = $userRepository->findBy(array('sendEmail' => true));
+            /** @var $sessionEmailer \Bethel\ReportViewBundle\Services\SessionEmail */
+            /** @var \Bethel\EntityBundle\Entity\User $emailRecipient */
+            foreach ($emailRecipients as $emailRecipient) {
+                // Make sure we're only sending to admins and professors
+                if ($this->userHasRole($emailRecipient, 'ROLE_ADMIN')) {
+                    $sessionEmailer = $this->sessionEmail->create($session, $emailRecipient);
+                    $sessionEmailer->sendEmail(null, true);
+                } elseif ($this->userHasRole($emailRecipient, 'ROLE_PROFESSOR')) {
+                    /** @var \Bethel\EntityBundle\Entity\CourseRepository $courseRepository */
+                    $courseRepository = $this->em->getRepository('BethelEntityBundle:Course');
 
-                // We only want to send professors an email if they teach a course
-                // that at least one student attended for at this session
-                if($reportCourses) {
-                    $sessionEmailer = $this->sessionEmail->create($session, $emailRecipient, $reportCourses);
-                    $sessionEmailer->sendEmail();
-                } else {
-                    continue;
+                    $reportCourses = array();
+                    $sessionCourses = $courseRepository->getAttendedSessionCourses($session);
+                    foreach ($sessionCourses as $course) {
+                        if ($course->getProfessors()->contains($emailRecipient))
+                            array_push($reportCourses, $course);
+                    }
+
+                    // We only want to send professors an email if they teach a course
+                    // that at least one student attended for at this session
+                    if ($reportCourses) {
+                        $sessionEmailer = $this->sessionEmail->create($session, $emailRecipient, $reportCourses);
+                        $sessionEmailer->sendEmail();
+                    } else {
+                        continue;
+                    }
                 }
             }
+            return $session;
         }
-        return $session;
+        return false;
     }
 
     private function userHasRole(User $user, $roleString) {
