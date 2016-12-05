8b8c4b84372d4dfde7e58f070d47ece2c0b48ecd
c-schwarze <ces55739@bethel.edu>
2016-12-05 13:47:34 -0600

    typo

--- src/Bethel/EntityBundle/Form/StudentAddAttendanceType.php
+++ src/Bethel/EntityBundle/Form/StudentAddAttendanceType.php
@@ -24,7 +24,6 @@ class StudentAddAttendanceType extends AbstractType
             ->add('student', 'entity', array(
                     'label' => 'Registered Students',
                     'class' => 'BethelEntityBundle:User',
-                    'style' => 'height:400px',
                     'multiple' => false,
                     'expanded' => false,
                     'required' => true,
@@ -37,7 +36,7 @@ class StudentAddAttendanceType extends AbstractType
                         );
                     },
                     'mapped' => true,
-                    'attr' => array('class'=>'chosen-select','data-placeholder'=>'Choose student ...')
+                    'attr' => array('class'=>'chosen-select','data-placeholder'=>'Choose student ...', 'style' => 'height:400px')
                 )
             )
             ->add('save','submit', array(
