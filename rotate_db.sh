decc10fe57f74b3037fbefe9e6e05f38dd9fd782
c-schwarze <ces55739@bethel.edu>
2017-10-11 10:10:48 -0500

    added script for db backups

---
+++ rotate_db.sh
@@ -0,0 +1,30 @@
+function parse_yaml {
+    local prefix=$2
+    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+    sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+    awk -F$fs '{
+        indent = length($1)/2;
+        vname[indent] = $2;
+        for (i in vname) {if (i > indent) {delete vname[i]}}
+        if (length($3) > 0) {
+            vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+            printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+        }
+    }'
+}
+
+#!/bin/bash
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+eval $(parse_yaml $SCRIPTPATH/app/config/parameters.yml)
+
+DATEVAR=`date "+%m-%d-%Y_%H%M"`
+if [ ! -d "db_back" ]; then
+  mkdir db_back
+fi
+mysqldump -u $parameters__database_user --password=$parameters__database_password $parameters__database_name > db_back/db.sql.back_$DATEVAR
+find $parameters__app_root/db_back/db.sql.back* -mtime +13 -type f -exec rm {} \;
