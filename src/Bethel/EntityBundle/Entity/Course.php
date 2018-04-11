d681cc2ba09d7a4cd707d68fcc443886e3b95619
c-schwarze <ces55739@bethel.edu>
2018-04-11 13:48:27 -0500

    Tried to remove the security value

--- src/Bethel/EntityBundle/Entity/Course.php
+++ src/Bethel/EntityBundle/Entity/Course.php
@@ -149,23 +149,17 @@ class Course
      */
     private $room;
 
-//    /**
-//     * @ORM\ManyToMany(targetEntity="User", inversedBy="courseViewer")
-//     * @ORM\JoinTable(
-//     *  name="CourseViewer",
-//     *  joinColumns={
-//     *      @ORM\JoinColumn(name="course_id", referencedColumnName="id")
-//     *  },
-//     *  inverseJoinColumns={
-//     *      @ORM\JoinColumn(name="user_id", referencedColumnName="id")
-//     *  }
-//     * )
-//     */
-
     /**
-     * @ORM\ManyToMany(targetEntity="User", mappedBy="course")
-     * @ORM\JoinTable(name="CourseViewer")
-     * @MaxDepth(1)
+     * @ORM\ManyToMany(targetEntity="User", inversedBy="courseViewer")
+     * @ORM\JoinTable(
+     *  name="CourseViewer",
+     *  joinColumns={
+     *      @ORM\JoinColumn(name="course_id", referencedColumnName="id")
+     *  },
+     *  inverseJoinColumns={
+     *      @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     *  }
+     * )
      */
     private $courseViewers;
 
